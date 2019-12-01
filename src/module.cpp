8abf4286c53810544b317c88e3b24d5d3f19fcff
Thomas Meister <su.thomas.meister@gmail.com>
2019-12-01 16:59:31 +0100

    Incorporate fuel weight in calculation

--- src/module.cpp
+++ src/module.cpp
@@ -2,8 +2,17 @@
 
 namespace module {
 
+namespace {
+    constexpr int calculateRequiredFuelImpl(int accumulator, int module_mass) {
+        const auto fuel = module_mass / 3 - 2;
+        return fuel > 0
+            ? calculateRequiredFuelImpl(accumulator+fuel, fuel)
+            : accumulator;
+    }
+}
+
 int calculateRequiredFuel(int module_mass) {
-    return module_mass / 3 - 2;
+    return calculateRequiredFuelImpl(0, module_mass);
 }
 
 }
--- src/module.test.cpp
+++ src/module.test.cpp
@@ -2,10 +2,17 @@
 #include <catch2/catch.hpp>
 #include "module.hpp"
 
-TEST_CASE("calculation of required fuel works", "[module,fuel]") {
+TEST_CASE("calculation of required fuel works", "[module], [fuel]") {
     using module::calculateRequiredFuel;
     REQUIRE(calculateRequiredFuel(12) == 2);
     REQUIRE(calculateRequiredFuel(14) == 2);
-    REQUIRE(calculateRequiredFuel(1969) == 654);
-    REQUIRE(calculateRequiredFuel(100756) == 33583);
+    REQUIRE(calculateRequiredFuel(1969) == 966);
+    REQUIRE(calculateRequiredFuel(100756) == 50346);
+}
+
+TEST_CASE("fuel calculation is capped at zero", "[module], [fuel]") {
+    using module::calculateRequiredFuel;
+    REQUIRE(calculateRequiredFuel(0) == 0);
+    REQUIRE(calculateRequiredFuel(2) == 0);
+    REQUIRE(calculateRequiredFuel(-2) == 0);
 }
