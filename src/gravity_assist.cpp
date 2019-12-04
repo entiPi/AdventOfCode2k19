45cd7cd97d6ba4d129ecaf5f699504ee546fbd12
Thomas Meister <su.thomas.meister@gmail.com>
2019-12-04 22:51:22 +0100

    Search for noun and verb

--- src/gravity_assist.cpp
+++ src/gravity_assist.cpp
@@ -1,4 +1,9 @@
 #include <iostream>
+#include <algorithm>
+#include <numeric>
+#include <iterator>
+#include <tuple>
+#include <array>
 #include <intcode/intcode.hpp>
 
 static const intcode::program original_prog{
@@ -13,12 +18,77 @@ static const intcode::program original_prog{
     0
 };
 
+namespace {
+
+template<typename First1, typename Last1, typename First2, typename Last2>
+struct cartesian_product_iterator
+    : std::iterator<std::input_iterator_tag, std::tuple<First1, First2>> {
+
+    using value_type = std::tuple<First1, First2>;
+
+    cartesian_product_iterator(First1 first1, Last1 last1, First2 first2, Last2 last2)
+        : f1{first1}, l1{last1}
+        , f2{first2}, l2{last2}
+        , c1{first1}, c2{first2} {
+    }
+
+    value_type operator*() const { return std::make_tuple(c1, c2); }
+
+    operator bool() const { return c2 < l2; }
+
+    value_type operator++() {
+        ++c1;
+        if (c1 == l1) {
+            c1 = f1;
+            ++c2;
+        }
+        return std::make_tuple(c1, c2);
+    }
+
+    value_type operator++(int) {
+        auto tmp{*this};
+        ++*this;
+        return *tmp;
+    }
+
+    private:
+    const First1 f1;
+    const Last1 l1;
+    const First2 f2;
+    const Last2 l2;
+    First1 c1;
+    First2 c2;
+};
+
+template<typename F1, typename L1, typename F2, typename L2>
+auto make_cartesian_product_iterator(F1 f1, L1 l1, F2 f2, L2 l2) {
+    return cartesian_product_iterator<F1,L1,F2,L2>{f1, l1, f2, l2};
+}
+
+}
 
 int main() {
-    auto fixed_prog{original_prog};
-    fixed_prog[1] = 12;
-    fixed_prog[2] = 2;
+    using namespace std;
+    using range_type = array<int,100>;
+    range_type number_range{};
+    iota(begin(number_range), end(number_range), 0);
 
-    const auto result = intcode::compute(std::move(fixed_prog));
-    std::cout << "calculated position0: " << result[0] << '\n';
+    auto const [noun, verb] = [&] {
+      for (auto number_seq = make_cartesian_product_iterator(
+              begin(number_range), end(number_range),
+              begin(number_range), end(number_range))
+          ; number_seq
+          ; ++number_seq) {
+          auto const& [noun, verb] = *number_seq;
+          auto fixed_prog{original_prog};
+          fixed_prog[1] = *noun;
+          fixed_prog[2] = *verb;
+          if (19690720 == intcode::compute(fixed_prog)[0])
+              return std::tie(*noun, *verb);
+      }
+      throw std::runtime_error{"verb, noun not found"};
+    }();
+
+    auto const result = 100 * noun + verb;
+    std::cout << "result: " << result << '\n';
 }
